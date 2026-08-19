3760b48854b4f17a790235d02e30bf9281838b65
0x1 <kevin.francart3@gmail.com>
2026-08-19 19:16:25 +0200

    misc

--- src/Data/Array.rs
+++ src/Data/Array.rs
@@ -2,172 +2,119 @@ use purust_core::*;
 use std::rc::Rc;
 
 pub fn Data_Array_length() -> UnknownType {
-    perceus_ptr::PerceusPtr::new(Record_a {
-        call: Some(Rc::new(move |mut xs: UnknownType| -> UnknownType {
-            let arr = xs.init_array.as_ref().unwrap();
+    crate::Value::Func(Rc::new(move |mut xs: UnknownType| -> UnknownType {
+            let arr = xs.unwrap_array();
             mk_int(arr.len() as i64)
-        })),
-        ..Default::default()
-    })
+        }))
 }
 
 pub fn Data_Array_unconsImpl() -> UnknownType {
-    perceus_ptr::PerceusPtr::new(Record_a {
-        call: Some(Rc::new(move |mut empty: UnknownType| -> UnknownType {
-            perceus_ptr::PerceusPtr::new(Record_a {
-                call: Some(Rc::new(move |mut next: UnknownType| -> UnknownType {
+    crate::Value::Func(Rc::new(move |mut empty: UnknownType| -> UnknownType {
+            crate::Value::Func(Rc::new(move |mut next: UnknownType| -> UnknownType {
                     let empty = empty.clone();
-                    perceus_ptr::PerceusPtr::new(Record_a {
-                        call: Some(Rc::new(move |mut xs: UnknownType| -> UnknownType {
+                    crate::Value::Func(Rc::new(move |mut xs: UnknownType| -> UnknownType {
                             let mut empty = empty.clone();
                             let mut next = next.clone();
-                            let arr = xs.init_array.as_ref().unwrap();
+                            let arr = xs.unwrap_array();
                             if arr.is_empty() {
-                                empty.call.clone().unwrap()(UnknownType::new(Record_a { ..Default::default() }))
+                                empty.unwrap_func()(crate::Value::Record(perceus_ptr::PerceusPtr::new(crate::Record_a { ..Default::default() })))
                             } else {
                                 let head = arr[0].clone();
                                 let tail = arr[1..].to_vec();
-                                next.call.clone().unwrap()(head).call.clone().unwrap()(crate::mk_array(tail))
+                                next.unwrap_func()(head).unwrap_func()(crate::mk_array(tail))
                             }
-                        })),
-                        ..Default::default()
-                    })
-                })),
-                ..Default::default()
-            })
-        })),
-        ..Default::default()
-    })
+                        }))
+                }))
+        }))
 }
 
 pub fn Data_Array_indexImpl() -> UnknownType {
-    perceus_ptr::PerceusPtr::new(Record_a {
-        call: Some(Rc::new(move |mut just: UnknownType| -> UnknownType {
-            perceus_ptr::PerceusPtr::new(Record_a {
-                call: Some(Rc::new(move |mut nothing: UnknownType| -> UnknownType {
+    crate::Value::Func(Rc::new(move |mut just: UnknownType| -> UnknownType {
+            crate::Value::Func(Rc::new(move |mut nothing: UnknownType| -> UnknownType {
                     let just = just.clone();
-                    perceus_ptr::PerceusPtr::new(Record_a {
-                        call: Some(Rc::new(move |mut xs: UnknownType| -> UnknownType {
+                    crate::Value::Func(Rc::new(move |mut xs: UnknownType| -> UnknownType {
                             let just = just.clone();
                             let nothing = nothing.clone();
-                            perceus_ptr::PerceusPtr::new(Record_a {
-                                call: Some(Rc::new(move |mut i: UnknownType| -> UnknownType {
+                            crate::Value::Func(Rc::new(move |mut i: UnknownType| -> UnknownType {
                                     let mut just = just.clone();
                                     let nothing = nothing.clone();
-                                    let arr = xs.init_array.as_ref().unwrap();
-                                    let idx_raw = i.init_int.unwrap();
+                                    let arr = xs.unwrap_array();
+                                    let idx_raw = i.unwrap_int();
                                     if idx_raw < 0 || idx_raw >= arr.len() as i64 {
                                         nothing
                                     } else {
-                                        just.call.clone().unwrap()(arr[idx_raw as usize].clone())
+                                        just.unwrap_func()(arr[idx_raw as usize].clone())
                                     }
-                                })),
-                                ..Default::default()
-                            })
-                        })),
-                        ..Default::default()
-                    })
-                })),
-                ..Default::default()
-            })
-        })),
-        ..Default::default()
-    })
+                                }))
+                        }))
+                }))
+        }))
 }
 
 pub fn Data_Array_sliceImpl() -> UnknownType {
-    perceus_ptr::PerceusPtr::new(Record_a {
-        call: Some(Rc::new(move |mut s: UnknownType| -> UnknownType {
-            perceus_ptr::PerceusPtr::new(Record_a {
-                call: Some(Rc::new(move |mut e: UnknownType| -> UnknownType {
+    crate::Value::Func(Rc::new(move |mut s: UnknownType| -> UnknownType {
+            crate::Value::Func(Rc::new(move |mut e: UnknownType| -> UnknownType {
                     let s = s.clone();
-                    perceus_ptr::PerceusPtr::new(Record_a {
-                        call: Some(Rc::new(move |mut l: UnknownType| -> UnknownType {
-                            let arr = l.init_array.as_ref().unwrap();
-                            let start = std::cmp::max(0, s.init_int.unwrap()) as usize;
-                            let end = std::cmp::min(arr.len() as i64, e.init_int.unwrap()) as usize;
+                    crate::Value::Func(Rc::new(move |mut l: UnknownType| -> UnknownType {
+                            let arr = l.unwrap_array();
+                            let start = std::cmp::max(0, s.unwrap_int()) as usize;
+                            let end = std::cmp::min(arr.len() as i64, e.unwrap_int()) as usize;
                             let start = std::cmp::min(start, arr.len());
                             let end = std::cmp::max(start, end);
                             let sliced = arr[start..end].to_vec();
                             crate::mk_array(sliced)
-                        })),
-                        ..Default::default()
-                    })
-                })),
-                ..Default::default()
-            })
-        })),
-        ..Default::default()
-    })
+                        }))
+                }))
+        }))
 }
 
 pub fn Data_Array_findIndexImpl() -> UnknownType {
-    perceus_ptr::PerceusPtr::new(Record_a {
-        call: Some(Rc::new(move |mut just: UnknownType| -> UnknownType {
-            perceus_ptr::PerceusPtr::new(Record_a {
-                call: Some(Rc::new(move |mut nothing: UnknownType| -> UnknownType {
+    crate::Value::Func(Rc::new(move |mut just: UnknownType| -> UnknownType {
+            crate::Value::Func(Rc::new(move |mut nothing: UnknownType| -> UnknownType {
                     let just = just.clone();
-                    perceus_ptr::PerceusPtr::new(Record_a {
-                        call: Some(Rc::new(move |mut f: UnknownType| -> UnknownType {
+                    crate::Value::Func(Rc::new(move |mut f: UnknownType| -> UnknownType {
                             let just = just.clone();
                             let nothing = nothing.clone();
-                            perceus_ptr::PerceusPtr::new(Record_a {
-                                call: Some(Rc::new(move |mut xs: UnknownType| -> UnknownType {
+                            crate::Value::Func(Rc::new(move |mut xs: UnknownType| -> UnknownType {
                                     let mut just = just.clone();
                                     let nothing = nothing.clone();
                                     let mut f = f.clone();
-                                    let arr = xs.init_array.as_ref().unwrap();
+                                    let arr = xs.unwrap_array();
                                     for (i, x) in arr.iter().enumerate() {
-                                        let res = f.call.clone().unwrap()(x.clone());
-                                        if res.init_bool.unwrap() {
-                                            return just.call.clone().unwrap()(mk_int(i as i64));
+                                        let res = f.unwrap_func()(x.clone());
+                                        if res.unwrap_bool() {
+                                            return just.unwrap_func()(mk_int(i as i64));
                                         }
                                     }
                                     nothing
-                                })),
-                                ..Default::default()
-                            })
-                        })),
-                        ..Default::default()
-                    })
-                })),
-                ..Default::default()
-            })
-        })),
-        ..Default::default()
-    })
+                                }))
+                        }))
+                }))
+        }))
 }
 
 pub fn Data_Array_filterImpl() -> UnknownType {
-    perceus_ptr::PerceusPtr::new(Record_a {
-        call: Some(Rc::new(move |mut f: UnknownType| -> UnknownType {
-            perceus_ptr::PerceusPtr::new(Record_a {
-                call: Some(Rc::new(move |mut xs: UnknownType| -> UnknownType {
+    crate::Value::Func(Rc::new(move |mut f: UnknownType| -> UnknownType {
+            crate::Value::Func(Rc::new(move |mut xs: UnknownType| -> UnknownType {
                     let mut f = f.clone();
-                    let arr = xs.init_array.as_ref().unwrap();
+                    let arr = xs.unwrap_array();
                     let mut res = Vec::new();
                     for x in arr.iter() {
-                        let ok = f.call.clone().unwrap()(x.clone());
-                        if ok.init_bool.unwrap() {
+                        let ok = f.unwrap_func()(x.clone());
+                        if ok.unwrap_bool() {
                             res.push(x.clone());
                         }
                     }
                     crate::mk_array(res)
-                })),
-                ..Default::default()
-            })
-        })),
-        ..Default::default()
-    })
+                }))
+        }))
 }
 
 pub fn Data_Array_rangeImpl() -> UnknownType {
-    perceus_ptr::PerceusPtr::new(Record_a {
-        call: Some(Rc::new(move |mut start: UnknownType| -> UnknownType {
-            perceus_ptr::PerceusPtr::new(Record_a {
-                call: Some(Rc::new(move |mut end: UnknownType| -> UnknownType {
-                    let s = start.init_int.unwrap();
-                    let e = end.init_int.unwrap();
+    crate::Value::Func(Rc::new(move |mut start: UnknownType| -> UnknownType {
+            crate::Value::Func(Rc::new(move |mut end: UnknownType| -> UnknownType {
+                    let s = start.unwrap_int();
+                    let e = end.unwrap_int();
                     let mut res = Vec::new();
                     if s <= e {
                         for i in s..=e {
@@ -181,10 +128,6 @@ pub fn Data_Array_rangeImpl() -> UnknownType {
                         }
                     }
                     crate::mk_array(res)
-                })),
-                ..Default::default()
-            })
-        })),
-        ..Default::default()
-    })
+                }))
+        }))
 }
